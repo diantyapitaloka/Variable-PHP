d90d43c99039374aad5cd8efc8e017948e49ec06
Diantya Pitaloka <147487436+diantyapitaloka@users.noreply.github.com>
2023-12-28 20:57:59 +0700

    update code

--- code.php
+++ code.php
@@ -1,2 +1,42 @@
-PHP is a programming language that has many variables. 
-Learning PHP is incomplete without discussing PHP variables.
+<?php
+  $nama = "Jason";
+  $a = 5;
+  $b = 10;
+?>
+
+<?php
+  $nama = "Jason";
+  echo "Perkenalkan nama saya adalah $nama";
+?>
+
+<?php
+  function belajar() {
+    $a = 5; // cakupan lokal
+    echo "Nilai variabel a dalam function adalah: $a";
+  } 
+  belajar();
+
+  // memanggil variabel a di luar function akan menghasilkan error
+  echo "Nilai variabel a di luar function adalah: $a";
+?>
+
+<?php
+  $a = 5; // cakupan global
+ 
+  function belajar() {
+    // memanggil variabel a di dalam function akan menghasilkan error
+    echo "Nilai variabel a dalam function adalah: $a";
+  } 
+  belajar();
+  echo "Nilai variabel a di luar function adalah: $a";
+?>
+
+<?php
+  function hitung() {
+    static $a = 5;
+    echo $a;
+    $a++;
+  }
+  hitung();
+  hitung();
+?>
